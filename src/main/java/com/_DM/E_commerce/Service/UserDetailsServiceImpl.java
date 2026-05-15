28f0af218cf1adee047c6bb5d02edd4a5ff1e99b
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:45:43 -0300

    src/main/java/com/_DM/E_commerce/Service/UserDetailsServiceImpl.java

---
+++ src/main/java/com/_DM/E_commerce/Service/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com._DM.E_commerce.Service;
+
+import com._DM.E_commerce.Repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UserDetailsServiceImpl(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado com o email: " + username));
+    }
+}
