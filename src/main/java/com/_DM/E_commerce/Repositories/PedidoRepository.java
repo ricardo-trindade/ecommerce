fc582be9bef5109d4dd64ec77703375d0ef817c2
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:45:19 -0300

    src/main/java/com/_DM/E_commerce/Repositories/PedidoRepository.java

---
+++ src/main/java/com/_DM/E_commerce/Repositories/PedidoRepository.java
@@ -0,0 +1,11 @@
+package com._DM.E_commerce.Repositories;
+
+import com._DM.E_commerce.Entity.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, UUID> {
+}
