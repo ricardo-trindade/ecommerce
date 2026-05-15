0e860e9c2c849d2b91de5f60f993ec7a97298262
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:48:13 -0300

    src/main/java/com/projeto/ecommerce/repositories/OrderRepository.java

--- src/main/java/com/projeto/ecommerce/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projeto.ecommerce.repositories;
-
-import com.projeto.ecommerce.entities.OrderEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface OrderRepository extends JpaRepository<OrderEntity, UUID> {
-}
