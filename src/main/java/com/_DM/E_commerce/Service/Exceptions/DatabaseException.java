8529647fc452bedebd40e9ec69ee1c7b5fbd7668
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:45:51 -0300

    src/main/java/com/_DM/E_commerce/Service/Exceptions/DatabaseException.java

---
+++ src/main/java/com/_DM/E_commerce/Service/Exceptions/DatabaseException.java
@@ -0,0 +1,12 @@
+package com._DM.E_commerce.Service.Exceptions;
+
+import java.io.Serial;
+
+public class DatabaseException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
