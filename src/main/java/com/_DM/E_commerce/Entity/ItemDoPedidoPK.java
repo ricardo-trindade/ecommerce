158daf44e8be50a865a083b98b19f2d1533d5c78
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:44:28 -0300

    src/main/java/com/_DM/E_commerce/Entity/ItemDoPedidoPK.java

---
+++ src/main/java/com/_DM/E_commerce/Entity/ItemDoPedidoPK.java
@@ -0,0 +1,25 @@
+package com._DM.E_commerce.Entity;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Embeddable
+@Getter
+@Setter
+@EqualsAndHashCode
+public class ItemDoPedidoPK implements Serializable {
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
+    private Produto produto;
+}
