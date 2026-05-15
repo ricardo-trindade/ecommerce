e65593bde92643ab54932180232057f30b3cf5d5
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:45:21 -0300

    src/main/java/com/_DM/E_commerce/Repositories/ProdutoRepository.java

---
+++ src/main/java/com/_DM/E_commerce/Repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com._DM.E_commerce.Repositories;
+
+import com._DM.E_commerce.Entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, UUID> {
+}
