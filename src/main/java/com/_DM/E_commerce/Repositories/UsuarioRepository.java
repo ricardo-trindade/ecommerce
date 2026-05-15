99868a73db0def3ff7d14c2c818c5615127f8492
ricardo-trindade <ricardotrindade.dev@gmail.com>
2026-05-15 10:45:25 -0300

    src/main/java/com/_DM/E_commerce/Repositories/UsuarioRepository.java

---
+++ src/main/java/com/_DM/E_commerce/Repositories/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com._DM.E_commerce.Repositories;
+
+import com._DM.E_commerce.Entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+    Optional<Usuario> findByEmail(String email);
+}
